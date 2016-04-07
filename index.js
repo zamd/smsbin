9d4dce7c52ab84731b26904308900192dab3313d
Zulfiqar Ahmed <zamd@users.noreply.github.com>
2016-04-07 21:11:05 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const url     = require('url');
 
 module.exports = (ctx, req, res) => {
     res.writeHead(statusCode, { 'Content-Type': 'application/json' });
-    res.end("{'message': 'welcome to auth0 extensions!'"}");
+    res.end("{'message': 'welcome to auth0 extensions!'}");
 }
